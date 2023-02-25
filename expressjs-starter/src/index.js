358d44463322024673271c7f89b83042acb7fe38
Luong Nguyen Hai Huy <76238604+huydeve@users.noreply.github.com>
2023-02-25 16:38:05 +0700

    Update index.js
    sadsa

--- expressjs-starter/src/index.js
+++ expressjs-starter/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-/* istanbul ignore next dsadds resdf*/
+/* istanbul ignore next resdf*/
 if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
