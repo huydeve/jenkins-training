c73b52b671541a27c74f1ebea76f39d21f7bf1a3
Luong Nguyen Hai Huy <76238604+huydeve@users.noreply.github.com>
2023-02-25 16:24:58 +0700

    Update index.js
    fix

--- expressjs-starter/src/index.js
+++ expressjs-starter/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-/* istanbul ignore next */
+/* istanbul ignore next resdf*/
 if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
